c91404bd1ecfcdc8771d1b6864806b022e7f8465
Ian Carbone <ian@affinitiquest.io>
2025-07-02 08:55:59 -0400

    Fixing version numbers.

--- Package.swift
+++ Package.swift
@@ -15,12 +15,12 @@ let package = Package(
     dependencies: [
     .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
 		.package(url: "https://github.com/crspybits/swift-log-file", from: "0.1.0"),
-		.package(url: "https://github.com/AffinitiQuest/eudi-lib-ios-iso18013-data-transfer.git", branch: "aq-dev"),
+		.package(url: "https://github.com/AffinitiQuest/eudi-lib-ios-iso18013-data-transfer.git", exact: "0.5.5-aq"),
 		//.package(path: "../eudi-lib-ios-iso18013-data-transfer"),
-		.package(url: "https://github.com/AffinitiQuest/eudi-lib-ios-wallet-storage.git", branch: "aq-dev"),
+		.package(url: "https://github.com/AffinitiQuest/eudi-lib-ios-wallet-storage.git", exact: "0.4.5-aq"),
     .package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-sdjwt-swift.git", exact: "0.5.1"),
-		.package(url: "https://github.com/AffinitiQuest/eudi-lib-ios-siop-openid4vp-swift.git", branch: "aq-dev"),
-		.package(url: "https://github.com/AffinitiQuest/eudi-lib-ios-openid4vci-swift.git", branch: "aq-dev"),
+		.package(url: "https://github.com/AffinitiQuest/eudi-lib-ios-siop-openid4vp-swift.git", exact: "0.7.0-aq"),
+		.package(url: "https://github.com/AffinitiQuest/eudi-lib-ios-openid4vci-swift.git", exact: "0.12.1-aq"),
 	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
